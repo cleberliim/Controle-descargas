f4028d946971657040f1b6c959734836e12c04ef
cleberliim <71719859+cleberliim@users.noreply.github.com>
2023-07-21 13:17:08 -0300

    modified:   inc/descargas.creater.php

--- inc/descargas.creater.php
+++ inc/descargas.creater.php
@@ -15,27 +15,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $peso = $_POST["peso"];
     $vp = $_POST["vp"];
     $qvp = $_POST["qvp"];
- 
+
 
     // Conecta ao banco de dados
     $conexao = mysqli_connect("localhost", "root", "", "comandopay");
-    
+
     // Verifica se a conexão foi estabelecida com sucesso
     if (mysqli_connect_errno()) {
         echo "Falha ao conectar ao MySQL: " . mysqli_connect_error();
         exit();
     }
-    
+
     // Prepara a consulta SQL
     $sql = "INSERT INTO descargas (CÓDIGO, cliente, filial, data_hora, vinculo, tipo, status, TITULAR, CPFCNPJ_DA_CONTA, VOLUMETONELADA, vp, QVP) VALUES ('$codigo', '$cliente', '$filial', '$data', '$vinculo', '$tipo', '$status',  '$motorista', '$cpfcnpj', '$peso', '$vp', '$qvp')";
 
     // Executa a consulta SQL
     if (mysqli_query($conexao, $sql)) {
-       header("Location: ../dashboard.php");
+        header("Location: ../recibo.php");
     } else {
         echo "Erro ao criar: " . mysqli_error($conexao);
     }
-    
+
     // Fecha a conexão com o banco de dados
     mysqli_close($conexao);
 }
