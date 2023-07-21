3b05ad9ae95f0294a584170873c2245c617420a2
cleberliim <71719859+cleberliim@users.noreply.github.com>
2023-07-21 11:43:06 -0300

    modified:   recibo.php

--- recibo.php
+++ recibo.php
@@ -4,23 +4,43 @@
 if (isset($_POST['buscar'])) {
     $id = $_POST['id'];
 
-    $query = "SELECT * FROM descargas WHERE id = $id";
+    $query = "SELECT * FROM descargas WHERE CÓDIGO = $id";
     $result = mysqli_query($conn, $query);
 
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
         $motorista = $row['TITULAR'];
         $cliente = $row['cliente'];
+        $cte = $row['cte'];
+        $valor = $row['VALOR'];
+        $filial = $row['filial'];
+        $peso = $row['peso'];
+        $qtd = $row['qtd'];
+        $vinculo = $row['vinculo'];
     } else {
-        // Caso não encontre o usuário, você pode definir valores padrão ou exibir uma mensagem de erro.
-        $motorista = '';
-        $cliente = '';
+        // Caso não encontre o código, você pode definir valores padrão ou exibir uma mensagem de erro.
+        $motorista = 'Sem registro';
+        $cliente = 'Sem registro';
+        $cte = 'Sem registro';
+        $valor = 'Sem registro';
+        $filial = 'Sem registro';
+        $saldo = 'Sem registro';
+        $peso = 'Sem registro';
+        $qtd = 'Sem registro';
+        $vinculo = 'Sem registro';
     }
 } else {
     // Caso o formulário não tenha sido enviado ainda, definir valores iniciais ou vazios.
     $id = '';
     $motorista = '';
     $cliente = '';
+    $cte = '';
+    $valor = '';
+    $filial = '';
+    $saldo = '';
+    $peso = '';
+    $qtd = '';
+    $vinculo = '';
 }
 
 ?>
@@ -49,20 +69,87 @@ if (isset($_POST['buscar'])) {
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="id" value="<?php echo $id; ?>">
                 <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit" name="buscar">Buscar</button>
             </div>
+
             <div class="w-full md:w-80 px-3 mb-4 md:mb-0">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="motorista">Motorista:</label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="motorista" value="<?php echo $motorista; ?>">
                 <br>
             </div>
-            <!-- Rest of the input fields... -->
-        </div>
 
-        <!-- Rest of the form... -->
 
-        <div class="flex justify-end">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">IMPRIMIR</button>
+            <div class="w-full md:w-80 px-3">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="cte">CTE:</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="cte" type="text" value="<?php echo $cte; ?>">
+            </div>
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="cliente">Cliente</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="cliente" type="text" value="<?php echo $cliente; ?>">
+            </div>
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="valor">Valor</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="valor" id="valor" type="text" value="<?php echo $valor; ?>">
+            </div>
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="filial">Filial</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="filial" type="text" value="<?php echo $filial; ?>">
+            </div>
+
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="peso">Peso:</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="peso" type="text" value="<?php echo $peso; ?>">
+            </div>
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="qtd">QTD Volume:</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="qtd" type="text" value="<?php echo $qtd; ?>">
+            </div>
+
+
+            <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="vinculo">Vinculo</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="vinculo" type="text" value="<?php echo $vinculo; ?>">
+            </div>
+
+            <!-- dados ban -->
+
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+
+                <div class="w-full md:w-full px-3 ">
+
+                    <p class=" shadow appearance-none border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="normas-procedimentos" rows="4">
+
+
+                        <b> NORMAS E PROCEDIMENTOS: </b> <br><br>
+
+                        01) A prestação de contas deste adiantamento para pagamento de descarga, deve ser feita imediatamente em sua chegada de viagem.
+                        <br><br>
+                        02) Fica extremamente proibido o acumulo de vales de descarga.
+                        <br><br>
+                        03) Todas descargas deverão ter recibos devidamente assinado com RG de quem recebeu e telefone para contato, sabendo que
+                        descargas em fazendas, os recibos além destas informações, deverão vir carimbado pela fazenda.
+                        <br><br>
+                        04) Salvo de recibo quando a descarga for feita em lojas ou depositos na cidade.
+                        <br><br>
+                        <b> ADVERTÊNCIA:</b> <br><br>
+                        A) A não prestação de contas deste adiantamento, arcará em desconto em folha e advertencia por escrito.
+
+                    </p>
+                </div>
+            </div>
         </div>
-    </form>
+</div>
+
+<!-- Rest of the form... -->
+
+<div class="flex justify-end">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">IMPRIMIR</button>
+</div>
+</form>
 </div>
 
 <?php include_once("footer.php") ?>
