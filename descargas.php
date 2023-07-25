7f8fef94454983214702a678c960b398e1d7a3d6
cleberliim <71719859+cleberliim@users.noreply.github.com>
2023-07-25 15:25:16 -0300

    modified:   base.php  	modified:   dashboard.php  	modified:   descargas.php  	modified:   edit.descargas.php  	modified:   inc/descargas.creater.php  	modified:   inc/process.edit.php

--- descargas.php
+++ descargas.php
@@ -62,7 +62,7 @@
             </div>
 
             <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
-                <label for="tipo" class="block text-gray-700 text-sm font-bold mb-2">Tipo:</label>
+                <label for="tipo" class="block text-gray-700 text-sm font-bold mb-2">Tipo de pagamento:</label>
                 <select id="tipo" name="tipo" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     <option>Descarga</option>
                     <option>Balsa</option>
@@ -78,7 +78,9 @@
 
             <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="vp">V/P</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="vp" id="vp" type="text" placeholder="Digite o V/P">
+                <select id="vp" name="vp" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <option>Peso</option>
+                    <option>QTD VOL.</option>
             </div>
 
             <div class="w-full md:w-80 px-3 mb-4 md:mb-0 mt-6">
